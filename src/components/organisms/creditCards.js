9b1dd5dace1257c327475f60e10740b3d682ca1a
lucaslfranco <llucasrgt@gmail.com>
2020-09-13 19:31:17 -0300

    feat: add credit cards page

---
+++ src/components/organisms/creditCards.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import styles from '../../assets/mainStyles';
+
+import {
+	Box,
+	Grid,
+	InputAdornment,
+	LinearProgress,
+	MenuItem,
+	Typography,
+	TextField,
+} from '@material-ui/core';
+
+import PaymentIcon from '@material-ui/icons/Payment';
+
+function CreditCards(props) {
+	const { classes } = props;
+
+	const [selectedCard, setSelectedCard] = useState(0);
+	const [filteredCards, setFilteredCards] = useState([]);
+	const cards = [
+		{ bankCode: 6, bank: 'Banco Safra S.A', type: 'Visa', number: 'XXXX XXXX XXXX 1234', limit: 100000, currentUse: 55000, available: 45000 },
+		{ bankCode: 5, bank: 'Santander', type: 'MasterCard', number: 'XYXY XYXY XYXY 9876', limit: 80000, currentUse: 20000, available: 60000 },
+	];
+	const [totalLimit, setTotalLimit] = useState(0);
+
+	useEffect(() => {
+		let filteredCards = cards;
+		if (props.institution)
+			filteredCards = cards.filter(card => card.bankCode === props.institution);
+
+		setTotalLimit(filteredCards.reduce((a, b) => a + (b['limit'] || 0), 0));
+		setFilteredCards(filteredCards);
+	}, [props.institution])
+
+	const getProgress = () => {
+		console.log('Progress')
+		const card = cards[selectedCard];
+		return 100 * card.currentUse / card.limit;
+	}
+
+	const formatter =
+		new Intl.NumberFormat('pt-BR', {
+			style: 'currency',
+			currency: 'BRL',
+			minimumFractionDigits: 2
+		});
+
+	return (
+		<Grid>
+			<Grid className={`${classes.container} ${classes.homeContainer}`}>
+				<Grid item xs={12} className={`${classes.indicator} indicatorBlue`}>
+					<Box align="center">
+						<Typography component="span" className={classes.indicatorValue} style={{ fontSize: 32 }}>
+							{formatter.format(totalLimit)}
+						</Typography>
+					</Box>
+					<Box align="center">
+						<Typography component="span" className={classes.indicatorLabel} style={{ fontSize: 22 }}>
+							Limite Total de Crédito
+						</Typography>
+					</Box>
+				</Grid>
+				<Grid container justify="center">
+					<Typography variant="h6" className="title" align="center">
+						Cartões de Crédito
+					</Typography>
+				</Grid>
+
+				<Grid container justify="center">
+					<TextField
+						id="creditCards"
+						InputProps={{
+							startAdornment:
+								<InputAdornment position="start">
+									<PaymentIcon />
+								</InputAdornment>,
+						}}
+						select
+						value={selectedCard}
+						onChange={({ target: { value } }) => setSelectedCard(value)}
+						variant="outlined"
+						style={{ marginBottom: 28 }}
+					>
+						{filteredCards.map((card, i) => (
+							<MenuItem key={i} value={i}>
+								{card.number}
+							</MenuItem>
+						))}
+					</TextField>
+
+					<Box mb={2} width={1}>
+						<Typography variant="h6" className="title" align="center" style={{ fontSize: 20 }}>Limite</Typography>
+					</Box>
+
+					<LinearProgress
+						variant="determinate"
+						value={getProgress()}
+						style={{ width: '100%', marginBottom: 4, height: 8, borderRadius: 2 }}
+					/>
+					<Grid container justify="space-between">
+						<Typography align="center" style={{ fontWeight: 'bold' }}>
+							Utilizado <br /> {formatter.format(cards[selectedCard].currentUse)}
+						</Typography>
+						<Typography align="center" style={{ fontWeight: 'bold' }}>
+							Disponível <br /> {formatter.format(cards[selectedCard].available)}
+						</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid >
+	)
+}
+
+export default withStyles(styles)(CreditCards);
