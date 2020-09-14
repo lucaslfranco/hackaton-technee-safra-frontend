208b1788ee3faf62b4b75b67b83174824dfa81c1
lucaslfranco <llucasrgt@gmail.com>
2020-09-13 22:17:04 -0300

    feat: add terms of use disclaimer

--- src/components/organisms/profileModal.js
+++ src/components/organisms/profileModal.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
-import { api } from '../../services';
-import { LANGUAGES } from '../../assets/languages';
+// import { api } from '../../services';
 
 import { Alert } from '../';
 
@@ -12,12 +11,9 @@ import combineStyles from '../../utils/combineStyles'
 import {
 	Box,
 	Button,
-	FormControl,
 	Grid,
-	MenuItem,
 	FormControlLabel,
 	Checkbox,
-	Select,
 	Snackbar,
 	TextField,
 	Typography,
@@ -33,6 +29,7 @@ function ProfileModal(props) {
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
 
+	const [step, setStep] = useState(1);
 	const [open, setOpen] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
 
@@ -40,8 +37,8 @@ function ProfileModal(props) {
 		position: 'relative',
 		// width: '32%',
 		minHeight: 300,
-		padding: 18,
-		borderRadius: 4,
+		padding: 32,
+		borderRadius: 2,
 		backgroundColor: 'white',
 		color: '#333',
 	}
@@ -60,125 +57,210 @@ function ProfileModal(props) {
 	return (
 		<Box
 			display='flex' justifyContent='center' alignItems='center' width={1} height={1}
-			style={{ backgroundColor: 'rgba(0,0,0,0.4)', position: 'fixed', top: 0, left: 0, zIndex: 5 }}
+			style={{ backgroundColor: 'rgba(0,0,0,0.4)', position: 'fixed', top: 0, left: 0, zIndex: 5, padding: 32 }}
 		>
 			<Grid container item xs={11} md={4} justify="center" style={modalStyle}>
-				<Typography variant="h4" align="center" style={{ padding: 12, fontWeight: "bold" }}>
-					Cadastro
+				{step === 1 ?
+					<>
+						<Typography variant="h4" align="center" style={{ padding: 12, fontWeight: "bold" }}>
+							Cadastro
 				</Typography>
+						<Grid item xs={11} md={9} style={{ padding: 12, paddingBottom: 20 }}>
 
-				<Grid item xs={11} md={9} style={{ padding: 12, paddingBottom: 20 }}>
+							<Grid component={'form'} container justify="space-between" onSubmit={registerUser}>
+								{/* <Box className={classes.closeModalBtn} onClick={props.closeModal}>X</Box> */}
 
-					<Grid component={'form'} container justify="space-between" onSubmit={registerUser}>
-						{/* <Box className={classes.closeModalBtn} onClick={props.closeModal}>X</Box> */}
+								<Box width={1} my={2}>
+									<TextField
+										variant="outlined"
+										id="fullname"
+										label="Nome Completo"
+										name="fullname"
+										autoComplete="fullname"
+										autoFocus
+										fullWidth
+										size="small"
+										value={fullName}
+										onChange={({ target: { value } }) => setFullName(value)}
+										required
+									/>
+								</Box>
 
-						<Box width={1} my={1}>
-							<TextField
-								variant="outlined"
-								id="fullname"
-								label="Nome Completo"
-								name="fullname"
-								autoComplete="fullname"
-								autoFocus
-								fullWidth
-								value={fullName}
-								onChange={({ target: { value } }) => setFullName(value)}
-								required
-							/>
-						</Box>
-
-						<Box width={1} mb={1}>
-							<TextField
-								variant="outlined"
-								id="birthdate"
-								label="Data de Nascimento"
-								name="birthdate"
-								autoComplete="birthdate"
-								fullWidth
-								value={birthDate}
-								onChange={({ target: { value } }) => setBirthDate(value)}
-								required
-							/>
-						</Box>
-						<Box width={1} mb={1}>
-							<TextField
-								variant="outlined"
-								id="cpf"
-								label="CPF"
-								name="cpf"
-								autoComplete="cpf"
-								fullWidth
-								value={cpf}
-								onChange={({ target: { value } }) => setCPF(value)}
-								required
-							/>
-						</Box>
-						<Box width={1} mb={1}>
-							<TextField
-								variant="outlined"
-								id="email"
-								label="E-mail"
-								name="email"
-								autoComplete="email"
-								fullWidth
-								value={username}
-								onChange={({ target: { value } }) => setUsername(value)}
-								required
-							/>
-						</Box>
-						<Box width={1} mb={1}>
-							<TextField
-								variant="outlined"
-								id="password"
-								label="Senha"
-								name="password"
-								autoComplete="password"
-								type="password"
-								fullWidth
-								value={password}
-								onChange={({ target: { value } }) => setPassword(value)}
-								required
-							/>
-						</Box>
-						<Box width={1} mb={1}>
-							<TextField
-								variant="outlined"
-								id="confirmpassword"
-								label="Confirme a senha"
-								name="confirmpassword"
-								autoComplete="confirm-password"
-								fullWidth
-								type="password"
-								value={confirmPassword}
-								onChange={({ target: { value } }) => setConfirmPassword(value)}
-								required
-							/>
-						</Box>
-						<FormControlLabel
-							control={<Checkbox value="remember" color="primary" required />}
-							label={<Box style={{ fontSize: 13 }}>Declaro que li e aceito os termos de uso</Box>}
-						/>
-						<Box width={1} mt={2}>
-							<Button
-								fullWidth
-								type="submit"
-								variant="contained"
-								color="primary"
-								style={{ backgroundColor: '#114263' }}
-							>
-								Criar cadastro
-						</Button>
-							<Box
-								mt={2}
-								className={classes.closeModalLink}
-								onClick={props.closeModal}
-								align="center"
-							>
-								Sair
+								<Box width={1} mb={2}>
+									<TextField
+										variant="outlined"
+										id="birthdate"
+										label="Data de Nascimento"
+										name="birthdate"
+										autoComplete="birthdate"
+										fullWidth
+										size="small"
+										value={birthDate}
+										onChange={({ target: { value } }) => setBirthDate(value)}
+										required
+									/>
+								</Box>
+								<Box width={1} mb={2}>
+									<TextField
+										variant="outlined"
+										id="cpf"
+										label="CPF"
+										name="cpf"
+										autoComplete="cpf"
+										fullWidth
+										size="small"
+										value={cpf}
+										onChange={({ target: { value } }) => setCPF(value)}
+										required
+									/>
+								</Box>
+								<Box width={1} mb={2}>
+									<TextField
+										variant="outlined"
+										id="email"
+										label="E-mail"
+										name="email"
+										autoComplete="email"
+										fullWidth
+										size="small"
+										value={username}
+										onChange={({ target: { value } }) => setUsername(value)}
+										required
+									/>
+								</Box>
+								<Box width={1} mb={2}>
+									<TextField
+										variant="outlined"
+										id="password"
+										label="Senha"
+										name="password"
+										autoComplete="password"
+										type="password"
+										fullWidth
+										size="small"
+										value={password}
+										onChange={({ target: { value } }) => setPassword(value)}
+										required
+									/>
+								</Box>
+								<Box width={1} mb={2}>
+									<TextField
+										variant="outlined"
+										id="confirmpassword"
+										label="Confirme a senha"
+										name="confirmpassword"
+										autoComplete="confirm-password"
+										fullWidth
+										size="small"
+										type="password"
+										value={confirmPassword}
+										onChange={({ target: { value } }) => setConfirmPassword(value)}
+										required
+									/>
+								</Box>
+								{/* <FormControlLabel
+									control={<Checkbox value="remember" color="primary" required />}
+									label={<Box style={{ fontSize: 13 }}>Declaro que li e aceito os termos de uso</Box>}
+								/> */}
+								<Box width={1} mt={2}>
+									<Button
+										fullWidth
+										type="submit"
+										variant="contained"
+										color="primary"
+										style={{ backgroundColor: '#114263' }}
+										onClick={() => setStep(2)}
+									>
+										Continuar
+									</Button>
+									<Box
+										mt={2}
+										className={classes.closeModalLink}
+										onClick={props.closeModal}
+										align="center"
+									>
+										Sair
 							</Box>
-						</Box>
-					</Grid>
-				</Grid>
+								</Box>
+							</Grid>
+						</Grid>
+					</> :
+					<>
+						<Typography variant="h5" align="center" style={{ padding: 12, fontWeight: "bold" }}>
+							Prezado(a) Cliente
+						</Typography>
+
+						<Typography align="center" style={{ fontSize: 14, lineHeight: 1.2, paddingLeft: 32, paddingRight: 32, fontWeight: "bold", marginTop: 12 }}>
+							Para o acesso é necessário nomear este
+							dispositivo e inserir o código enviado para o
+							telefone cadastrado.
+						</Typography>
+						<Grid item xs={11} md={9} style={{ padding: 12, paddingBottom: 20 }}>
+							<Grid component={'form'} container justify="space-between" onSubmit={registerUser}>
+								{/* <Box className={classes.closeModalBtn} onClick={props.closeModal}>X</Box> */}
+
+								<Box width={1} my={2}>
+									<TextField
+										variant="outlined"
+										id="fullname"
+										label="Nome do dispositivo"
+										name="fullname"
+										autoComplete="fullname"
+										autoFocus
+										fullWidth
+										size="small"
+										value={fullName}
+										onChange={({ target: { value } }) => setFullName(value)}
+										required
+									/>
+								</Box>
+								<Box width={1} mb={2}>
+									<TextField
+										variant="outlined"
+										id="password"
+										label="Código"
+										name="password"
+										autoComplete="password"
+										type="password"
+										fullWidth
+										size="small"
+										value={password}
+										onChange={({ target: { value } }) => setPassword(value)}
+										required
+									/>
+								</Box>
+
+								<Typography align="center" style={{ fontSize: 12, lineHeight: 1.3, fontWeight: "500", marginTop: 16, marginBottom: 16, marginLeft: -32, marginRight: -32 }}>
+									O cadastramento do computador é um recurso adicional de segurança, que permite realizar transações, identificando o computador do usuário. Este recurso adicional não dispensa os cuidados usuais para acesso à internet, como uso de programas antivírus atualizados e firewall.
+								</Typography>
+
+								<FormControlLabel
+									control={<Checkbox value="remember" color="primary" required />}
+									label={<Box style={{ fontSize: 13 }}>Declaro que li e aceito os termos de uso</Box>}
+								/>
+								<Box width={1} mt={2}>
+									<Button
+										fullWidth
+										type="submit"
+										variant="contained"
+										color="primary"
+										style={{ backgroundColor: '#114263' }}
+									>
+										Criar cadastro
+									</Button>
+									<Box
+										mt={2}
+										className={classes.closeModalLink}
+										onClick={props.closeModal}
+										align="center"
+									>
+										Sair
+									</Box>
+								</Box>
+							</Grid>
+						</Grid>
+					</>
+				}
 			</Grid>
 			<Snackbar
 				anchorOrigin={{
